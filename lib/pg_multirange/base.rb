272d20967cbf7b2b5f5f06b9a5f88dd2b351f291
Daniel Ackerman <daniel.joseph.ackerman@gmail.com>
2023-04-07 11:25:12 -0500

    Added ModuleBuilder pattern cache
    * In Rails & other multithreaded environments, it's possible that these
    anonymous modules can be created multiple times.
    
    * Using a mutex, only on the instantiation, it's possible to ensure a given
    "#{column_name}:#{value_class}" is only created once per iteration.

--- lib/pg_multirange/base.rb
+++ lib/pg_multirange/base.rb
@@ -4,23 +4,43 @@ require_relative 'value'
 require_relative 'model'
 
 module PgMultirange
+  # Adds in caching to prevent module space pollution. Builder pattern can lead to multiple modules existing.
+  @@module_cache = {}
+  @@module_cache_mutex = Mutex.new
+
+  def self.new_model_module(column_name, value_class)
+    @@module_cache_mutex.synchronize do
+      @@module_cache["#{column_name}:#{value_class}"] ||=
+        begin
+          PgMultirange::Model.build_module(
+            column_name,
+            value_class.new
+          )
+        end
+    end
+  end
+
   module Base
     extend ActiveSupport::Concern
 
     class_methods do
-      def timestamp_multirange(column_name)
-        value_class = PgMultirange::Value::Timestamp.new
+      def ts_multirange(column_name)
         send(
           :include,
-          PgMultirange::Model.build_module(column_name, value_class)
+          PgMultirange.new_model_module(
+            column_name,
+            PgMultirange::Value::Timestamp
+          )
         )
       end
 
-      def integer_multirange(column_name)
-        value_class = PgMultirange::Value::Integer.new
+      def int_multirange(column_name)
         send(
           :include,
-          PgMultirange::Model.build_module(column_name, value_class)
+          PgMultirange.new_model_module(
+            column_name,
+            PgMultirange::Value::Integer
+          )
         )
       end
     end
