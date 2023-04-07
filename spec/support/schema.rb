5bbaac4aa2eefbc99d5389d9ac06dbb3d085a69f
Daniel Ackerman <daniel.joseph.ackerman@gmail.com>
2023-04-07 11:32:36 -0500

    Added the remaining multirange columns to the test schema

--- spec/support/schema.rb
+++ spec/support/schema.rb
@@ -3,4 +3,12 @@ ActiveRecord::Schema.define do
 
   create_table "calendars", force: :cascade
   add_column :calendars, :availability, :tsmultirange
+
+  add_column :calendars, :epochs, :int4multirange
+  add_column :calendars, :big_int_epochs, :int8multirange
+
+  add_column :calendars, :meeting_durations, :nummultirange
+
+  add_column :calendars, :international_availability, :tstzmultirange
+  add_column :calendars, :bookable_days, :datemultirange
 end
